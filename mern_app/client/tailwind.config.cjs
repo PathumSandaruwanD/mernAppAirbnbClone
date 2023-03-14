39e19397f2f64e63f161fd740f09ac69cd216e41
Pathum Sandaruwan <85733563+PathumSandaruwanD@users.noreply.github.com>
2023-03-14 13:35:29 +0530

    initial commit

--- mern_app/client/tailwind.config.cjs
+++ mern_app/client/tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f5385D",
+      },
+    },
   },
   plugins: [],
 }
