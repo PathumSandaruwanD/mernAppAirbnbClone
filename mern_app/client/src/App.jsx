363cd669fd01a2ff6b1091ff9841b2fb4cebb90c
Pathum Sandaruwan <85733563+PathumSandaruwanD@users.noreply.github.com>
2023-03-14 13:34:08 +0530

    initial commit

---
+++ mern_app/client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <div>
+      <header className='p-4'>
+        <a href='#' className='flex items-center gap-1'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate-90">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+          </svg>
+          <span className='text-bold text-xl'>AIRBNB</span>
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
