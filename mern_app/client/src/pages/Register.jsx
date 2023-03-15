7e8f86d584c20e682bd7df77a1629d49445eeb34
Pathum Sandaruwan <85733563+PathumSandaruwanD@users.noreply.github.com>
2023-03-16 01:49:18 +0530

    Register page is done

--- mern_app/client/src/pages/Register.jsx
+++ mern_app/client/src/pages/Register.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Login = () => {
+
+const Register = () => {
   return (
     <div className='mt-4 grow flex item-center justify-around'>
       <div className='pt-40'>
-        <h1 className=' text-2xl text-center mb-4'>Login</h1>
+        <h1 className=' text-2xl text-center mb-4'>Register</h1>
         <form className='max-w-md  mx-auto'>
+          <input type='text' placeholder='Jone Doe'/>  
           <input type="email" placeholder='your@gmail.com' />
           <input type="password" placeholder='password' />
           <button className='primary mt-1'>Login</button>
           <div className='text-center py-2 text-gray-500'>
-            Don't have a account yet? 
-            <Link to="/register" className='underline text-black'>Register Now</Link>
+           Allready a member? 
+            <Link to="/login" className='underline text-black'>Login</Link>
           </div>
         </form>
       </div>
@@ -20,4 +22,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Register
